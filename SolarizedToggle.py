cc44d7a8734fe761d90790635ae8ab2395fe7820
David McCullough <damccull@gmail.com>
2013-01-20 13:42:13 -0600

    Fixes to make plugin change entire program color scheme instead of just current tab.

--- SolarizedToggle.py
+++ SolarizedToggle.py
@@ -1,11 +1,15 @@
 import sublime, sublime_plugin
 
-class SolarizedToggle(sublime_plugin.TextCommand):
-	def run(self, edit, **args):
+class SolarizedToggle(sublime_plugin.ApplicationCommand):
+	def run(self, **args):
+		settingsFile = "Preferences.sublime-settings"
 		schemeDay = args["color_sceme_day"]
 		schemeNight = args["color_sceme_night"]
 
-		current_scheme = self.view.settings().get("color_scheme")
-
+		settings = sublime.load_settings(settingsFile)
+		current_scheme = settings.get("color_scheme")
+		print "Current_scheme:" + str(current_scheme)
 		new_scheme = schemeDay if current_scheme == schemeNight else schemeNight
-		self.view.settings().set("color_scheme",new_scheme)
+
+		settings.set("color_scheme",new_scheme)
+		sublime.save_settings(settingsFile)
