eda961c4ba659e328b1bd70265a77351f17970b8
Stefan Pfenninger <stefan@pfenninger.org>
2013-03-27 14:13:52 +0000

    Refactoring; Added ability to change theme

--- SolarizedToggle.py
+++ SolarizedToggle.py
@@ -1,15 +1,22 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 class SolarizedToggle(sublime_plugin.ApplicationCommand):
-	def run(self, **args):
-		settingsFile = "Preferences.sublime-settings"
-		schemeDay = args["color_sceme_day"]
-		schemeNight = args["color_sceme_night"]
+    def flip(self, args, settings, what):
+        current = settings.get(what)
+        light = args["{}_light".format(what)]
+        dark = args["{}_dark".format(what)]
+        if current == dark:
+            new = light
+        else:
+            new = dark
+        settings.set(what, new)
+        return None
 
-		settings = sublime.load_settings(settingsFile)
-		current_scheme = settings.get("color_scheme")
-		print("Current_scheme:" + str(current_scheme))
-		new_scheme = schemeDay if current_scheme == schemeNight else schemeNight
-
-		settings.set("color_scheme",new_scheme)
-		sublime.save_settings(settingsFile)
+    def run(self, **args):
+        settings = sublime.load_settings("Preferences.sublime-settings")
+        self.flip(args, settings, 'color_scheme')
+        if args["flip_theme"]:
+            self.flip(args, settings, 'theme')
+        sublime.save_settings(settings)
