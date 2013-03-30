0c6e1ad84e632b41c443a30d6971f040c7ec4903
Stefan Pfenninger <stefan@pfenninger.org>
2013-03-30 09:04:45 +0100

    Further rewriting
    * Main script rewritten
    * Separate settings file
    * Menu entries for settings
    * Ability to dynamically override view settings

--- SolarizedToggle.py
+++ SolarizedToggle.py
@@ -2,21 +2,53 @@ import sublime
 import sublime_plugin
 
 
-class SolarizedToggle(sublime_plugin.ApplicationCommand):
-    def flip(self, args, settings, what):
-        current = settings.get(what)
-        light = args["{}_light".format(what)]
-        dark = args["{}_dark".format(what)]
-        if current == dark:
-            new = light
-        else:
-            new = dark
-        settings.set(what, new)
-        return None
+class SolarizedToggle(object):
+    def plugin_loaded_setup(self):
+        self.active_flip = False
+        self.global_settings = sublime.load_settings('Preferences.sublime-settings')
+        self.plugin_settings = sublime.load_settings('SolarizedToggle.sublime-settings')
+        self.state = self.plugin_settings.get('default_flipped_state')
+        self.override_views = self.plugin_settings.get('override_views')
 
+    def _set(self, setting, state):
+        new = self.plugin_settings.get("{}_{}".format(setting, state))
+        self.global_settings.set(setting, new)
+
+    def _flip_state(self):
+        if self.state == 'dark':
+            self.state = 'light'
+        else:
+            self.state = 'dark'
+
+    def flip(self):
+        self._flip_state()
+        self._set('color_scheme', self.state)
+        if self.plugin_settings.get("flip_theme"):
+            self._set('theme', self.state)
+        self.active_flip = True
+        self.update_view()
+
+    def update_view(self, view=None):
+        if self.active_flip and self.override_views:
+            desired_scheme = (self.plugin_settings
+                              .get("color_scheme_{}"
+                              .format(self.state)))
+            (sublime.active_window().active_view()
+             .settings().set('color_scheme', desired_scheme))
+
+
+class SolarizedToggleListener(sublime_plugin.EventListener):
+    def on_activated(self, view):
+        _flipper.update_view(view)
+
+
+class SolarizedToggleCommand(sublime_plugin.ApplicationCommand):
     def run(self, **args):
-        settings = sublime.load_settings("Preferences.sublime-settings")
-        self.flip(args, settings, 'color_scheme')
-        if args["flip_theme"]:
-            self.flip(args, settings, 'theme')
-        sublime.save_settings(settings)
+        _flipper.flip()
+
+
+_flipper = SolarizedToggle()
+
+
+def plugin_loaded():
+    _flipper.plugin_loaded_setup()
