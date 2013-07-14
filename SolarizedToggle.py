05153a0b20d3e9a3dec95384189a5fd9161d19dd
Stefan Pfenninger <stefan@pfenninger.org>
2013-07-14 10:47:51 +0200

    Minor improvements
    * Store settings globally again to get around load delays
    * Update all views on load to ensure desired state is set

--- SolarizedToggle.py
+++ SolarizedToggle.py
@@ -4,13 +4,14 @@ import sublime_plugin
 
 class SolarizedToggle(object):
     def plugin_loaded_setup(self):
-        self.global_settings = sublime.load_settings('Preferences.sublime-settings')
+        self.global_settings_file = 'Preferences.sublime-settings'
+        self.global_settings = sublime.load_settings(self.global_settings_file)
         self.plugin_settings_file = 'SolarizedToggle.sublime-settings'
         self.plugin_settings = sublime.load_settings(self.plugin_settings_file)
         self.state = self.plugin_settings.get('current_state')
 
     def _set(self, setting, state):
-        new = self.plugin_settings.get("{}_{}".format(setting, state))
+        new = self.plugin_settings.get('{}_{}'.format(setting, state))
         self.global_settings.set(setting, new)
 
     def _flip_state(self):
@@ -24,20 +25,26 @@ class SolarizedToggle(object):
     def flip(self):
         self._flip_state()
         self._set('color_scheme', self.state)
-        if self.plugin_settings.get("flip_theme"):
+        if self.plugin_settings.get('flip_theme'):
             self._set('theme', self.state)
+        if self.plugin_settings.get('update_global_settings'):
+            sublime.save_settings(self.global_settings_file)
         self.update_view()
 
     def update_view(self, view=None):
+        if not view:
+            view = sublime.active_window().active_view()
         desired_scheme = (self.plugin_settings
-                          .get("color_scheme_{}"
+                          .get('color_scheme_{}'
                           .format(self.state)))
-        (sublime.active_window().active_view()
-         .settings().set('color_scheme', desired_scheme))
+        view.settings().set('color_scheme', desired_scheme)
 
 
 class SolarizedToggleListener(sublime_plugin.EventListener):
     def on_activated(self, view):
+        _flipper.update_view(view)
+
+    def on_load(self, view):
         _flipper.update_view(view)
 
 
@@ -51,3 +58,8 @@ _flipper = SolarizedToggle()
 
 def plugin_loaded():
     _flipper.plugin_loaded_setup()
+    # Ensure that desired state is set for theme and all views
+    _flipper._set('theme', _flipper.state)
+    for w in sublime.windows():
+        for v in w.views():
+            _flipper.update_view(v)
